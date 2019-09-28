d0e42a78736a4e44fb3c8cd06ca7e82084815f45
NikitaMakovey <niks.finn@gmail.com>
2019-09-28 23:54:02 +1000

    Initial commit
    Realisation of PostgreSQL query and connection in C# code -> console output.

---
+++ ConsoleAppSQL/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using Npgsql;
+
+namespace ConsoleAppSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "Host=localhost;Username=postgres;Password=Aa3sdf&&;Database=postgres";
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+
+                var databaseTable = "simplenumbers";
+                
+                using (var cmd = new NpgsqlCommand())
+                {
+                    cmd.Connection = connection;
+                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS " + databaseTable +
+                        " (id SERIAL PRIMARY KEY, number INTEGER NOT NULL UNIQUE)";
+                    cmd.ExecuteNonQuery();
+                }
+                
+                var arrayNumbers = new ArrayList { 17, 27, 37, 43, 10, 15, 16, 23, 88};
+                foreach (var number in arrayNumbers)
+                {
+                    using (var cmd = new NpgsqlCommand())
+                    {
+                        cmd.Connection = connection;
+                        cmd.CommandText = "INSERT INTO " + databaseTable +
+                                          " (number) VALUES (" + number + ") ON CONFLICT (number) DO NOTHING";
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+
+                using (var cmd = new NpgsqlCommand("SELECT * FROM " + databaseTable, connection))
+                {
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        Console.WriteLine("|---------------|---------------|");
+                        Console.WriteLine("|       id      |     number    |");
+                        Console.WriteLine("|---------------|---------------|");
+                        while (reader.Read())
+                        {
+                            Console.Write("|\t{0}\t|\t{1}\t|\n", reader[0], reader[1]);
+                            Console.WriteLine("|---------------|---------------|");
+                        }
+                    }   
+                }
+            }
+        }
+    }
+}
